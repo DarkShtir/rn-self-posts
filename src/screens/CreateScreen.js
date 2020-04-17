7409a84ba03ab2c480c5701440f718779eb65fed
Drew Muliou <andreyjedai@tut.by>
2020-04-17 18:36:03 +0300

    Created Data base, added to add images and Full CRUD in DB

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import {
 	StyleSheet,
 	Text,
 	View,
 	TextInput,
-	Image,
 	Button,
 	ScrollView,
 	TouchableWithoutFeedback,
@@ -15,23 +14,26 @@ import { useDispatch } from 'react-redux';
 import { AppHeaderIcon } from '../components/AppHeaderIcon';
 import { THEME } from '../theme';
 import { addPost } from '../store/actions/post';
+import { PhotoPicker } from '../components/PhotoPicker';
 
 export const CreateScreen = ({ navigation }) => {
 	const dispatch = useDispatch();
 	const [text, setText] = useState('');
-
-	const img =
-		'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg';
+	const imgRef = useRef();
 
 	const saveHandler = () => {
 		const post = {
 			date: new Date().toJSON(),
 			text: text,
-			img: img,
+			img: imgRef.current,
 			booked: false,
 		};
 		dispatch(addPost(post));
 		navigation.navigate('Main');
+	};
+
+	const PhotoPickHandler = (uri) => {
+		imgRef.current = uri;
 	};
 
 	return (
@@ -46,16 +48,12 @@ export const CreateScreen = ({ navigation }) => {
 						onChangeText={setText}
 						multiline
 					/>
-					<Image
-						style={{ width: '100%', height: 200, marginBottom: 10 }}
-						source={{
-							uri: img,
-						}}
-					/>
+					<PhotoPicker onPick={PhotoPickHandler} />
 					<Button
 						title='Create post'
 						color={THEME.MAIN_COLOR}
 						onPress={saveHandler}
+						disabled={!text}
 					/>
 				</View>
 			</TouchableWithoutFeedback>
