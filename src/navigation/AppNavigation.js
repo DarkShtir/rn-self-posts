72f593df2a828773c821d45232bab67d19ee7b3a
Drew Muliou <Andreyjedai@tut.by>
2020-04-17 13:58:37 +0300

    Added navigation, all static screens, drawer(menu)

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,9 +1,26 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { BookedScreen } from '../screens/BookedScreen';
+import { AboutScreen } from '../screens/AboutScreen';
+import { CreateScreen } from '../screens/CreateScreen';
 import { THEME } from '../theme';
+
+const navigatorOptions = {
+	defaultNavigationOptions: {
+		headerStyle: {
+			backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+		},
+		headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+	},
+};
 
 const PostNavigator = createStackNavigator(
 	{
@@ -12,15 +29,102 @@ const PostNavigator = createStackNavigator(
 			screen: PostScreen,
 		},
 	},
+	navigatorOptions
+);
+
+const BookedNavigator = createStackNavigator(
 	{
-		initialRouteName: 'Main',
-		defaultNavigationOptions: {
-			headerStyle: {
-				backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+		Booked: BookedScreen,
+		Post: PostScreen,
+	},
+	navigatorOptions
+);
+
+const bottomTabsConfig = {
+	Post: {
+		screen: PostNavigator,
+		navigationOptions: {
+			tabBarLabel: 'All',
+			tabBarIcon: (info) => (
+				<Ionicons name='ios-albums' size={25} color={info.tintColor} />
+			),
+		},
+	},
+	Booked: {
+		screen: BookedNavigator,
+		navigationOptions: {
+			tabBarLabel: 'Bookmarks',
+			tabBarIcon: (info) => (
+				<Ionicons name='ios-star' size={25} color={info.tintColor} />
+			),
+		},
+	},
+};
+
+const BottomNavigator =
+	Platform.OS === 'android'
+		? createMaterialBottomTabNavigator(bottomTabsConfig, {
+				activeTintColor: '#fff',
+				shifting: true,
+				barStyle: {
+					backgroundColor: THEME.MAIN_COLOR,
+				},
+		  })
+		: createBottomTabNavigator({
+				bottomTabsConfig,
+				tabBarOptions: {
+					activeTintColor: THEME.MAIN_COLOR,
+				},
+		  });
+
+const AboutNavigator = createStackNavigator(
+	{
+		About: {
+			screen: AboutScreen,
+		},
+	},
+	navigatorOptions
+);
+
+const CreateNavigator = createStackNavigator(
+	{
+		Create: {
+			screen: CreateScreen,
+		},
+	},
+	navigatorOptions
+);
+
+const MainNavigator = createDrawerNavigator(
+	{
+		PostTabs: {
+			screen: BottomNavigator,
+			navigationOptions: {
+				drawerLabel: 'Main page',
+				// drawerIcon: <Ionicons name='ios-star' />,
 			},
-			headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+		},
+		About: {
+			screen: AboutNavigator,
+			navigationOptions: {
+				drawerLabel: 'About app',
+			},
+		},
+		Create: {
+			screen: CreateNavigator,
+			navigationOptions: {
+				drawerLabel: 'Create post',
+			},
+		},
+	},
+	{
+		contentOptions: {
+			activeTintColor: THEME.MAIN_COLOR,
+			labelStyle: {
+				fontFamily: 'open-bold',
+			},
 		},
 	}
 );
 
-export const AppNavigation = createAppContainer(PostNavigator);
+export const AppNavigation = createAppContainer(MainNavigator);
--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppHeaderIcon } from '../components/AppHeaderIcon';
 
 export const AboutScreen = () => {
 	return (
 		<View style={styles.center}>
-			<Text>About screen</Text>
+			<Text>This is best application for your post!</Text>
+			<Text>
+				Version: <Text style={styles.version}>1.0.0</Text>
+			</Text>
 		</View>
 	);
 };
+
+AboutScreen.navigationOptions = ({ navigation }) => ({
+	headerTitle: 'About Application',
+	headerLeft: () => (
+		<HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+			<Item
+				title='Toggle Drawer'
+				iconName='ios-menu'
+				onPress={() => navigation.toggleDrawer()}
+			/>
+		</HeaderButtons>
+	),
+});
 
 const styles = StyleSheet.create({
 	center: {
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
+	},
+	version: {
+		fontFamily: 'open-bold',
 	},
 });
